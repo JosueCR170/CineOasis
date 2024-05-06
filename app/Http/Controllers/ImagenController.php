eb7340f8765bcb0cc0c0c7a212e9f71b04670110
JosueCR170 <josolorzano4@gmail.com>
2024-05-05 20:57:45 -0600

    imagenes-falta update

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Imagen;
+// use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ImagenController extends Controller
 {
@@ -18,6 +20,8 @@ class ImagenController extends Controller
         );
         return response()->json($response,200);
     }
+    
+ 
     public function store(Request $request){
         $data_input = $request->input('data', null);
         
@@ -30,8 +34,8 @@ class ImagenController extends Controller
                 $data = array_map('trim', $data);
     
                 $rules = [
-                    'idPelicula' => 'required',
-                    'descripcion' => 'required',
+                    'idPelicula' => 'required|exists:peliculas,id',
+                    'descripcion' => 'required'
                 ];
     
                 $isValid = \validator($data, $rules);
@@ -39,7 +43,7 @@ class ImagenController extends Controller
                 if(!$isValid->fails()){
                     $imagen = new Imagen();
                     $imagen->idPelicula = $data['idPelicula']; 
-                    $imagen->imagen = $image->store('imagenes/pelicula'); // Guardar la imagen en el sistema de archivos
+                    $imagen->imagen = base64_encode(file_get_contents($image)); 
                     $imagen->descripcion = $data['descripcion'];
     
                     $imagen->save();
@@ -72,44 +76,25 @@ class ImagenController extends Controller
     }
     
 
-        // public function show($id){
-        //     $data=Imagen::find($id);
-        //     if(is_object($data)){
-        //         $response=array(
-        //         'status'=>200,
-        //         'menssage'=>'Imagen encontrada',
-        //         'category'=>$data
-        //         );
-        //     }
-        //     else{
-        //         $response = array(
-        //             'status'=>404,
-        //             'menssage'=>'Recurso no encontrado'
-        //         );
-
-        //     }
-        //     return response()->json($response,$response['status']);
-        // }
-        public function show($id) {
-            $imagen = Imagen::find($id);
-        
-            if ($imagen) {
-                // Decodificar el blob de la imagen
-                $imageData = base64_decode($imagen->imagen);
-        
-                // Establecer el tipo de contenido de la respuesta
-                $headers = [
-                    'Content-Type' => 'image/png', // Cambia el tipo de contenido según el formato de la imagen
-                    'Content-Length' => strlen($imageData)
-                ];
-        
-                // Devolver la imagen como respuesta HTTP
-                return response($imageData, 200, $headers);
-            } else {
-                // Si la imagen no se encuentra, devolver una respuesta 404
-                return response()->json(['message' => 'Imagen no encontrada'], 404);
+        public function show($id){
+            $data=Imagen::find($id);
+            if(is_object($data)){
+                $response=array(
+                'status'=>200,
+                'menssage'=>'Imagen encontrada',
+                'category'=>$data
+                );
             }
+            else{
+                $response = array(
+                    'status'=>404,
+                    'menssage'=>'Recurso no encontrado'
+                );
+
+            }
+            return response()->json($response,$response['status']);
         }
+      
         
 
 
@@ -136,5 +121,56 @@ class ImagenController extends Controller
             return response()->json($response,$response['status']);
         }
 
-
+    public function update(Request $request, $id) {
+        $imagen = Imagen::find($id);
+    
+        if (!$imagen) {
+            $response = [
+                'status' => 404,
+                'message' => 'Imagen no encontrada'
+            ];
+            return response()->json($response, $response['status']);
+        }
+    
+        $data_input = $request->input('data', null);
+        $image_input = $request->file('imagen');
+    
+        if ($data_input || $image_input) {
+            $data = json_decode($data_input, true) ?: [];
+            $data = array_map('trim', $data);
+    
+            $rules = [
+                'idPelicula' => 'exists:peliculas,id'
+            ];
+    
+            $isValid = \validator($data, $rules);
+    
+            if(!$isValid->fails()) {
+                $imagen->idPelicula = $data['idPelicula']; 
+                $imagen->descripcion = $data['descripcion'];
+                $imagen->imagen = base64_encode(file_get_contents($image_input)); 
+    
+                $imagen->save();
+                $response = [
+                    'status' => 200,
+                    'message' => 'Imagen actualizada',
+                    'imagen' => $imagen
+                ];
+            } else {
+                $response = [
+                    'status' => 406,
+                    'message' => 'Datos inválidos',
+                    'errors' => $isValid->errors()
+                ];
+            }
+        } else {
+            $response = [
+                'status' => 400,
+                'message' => 'No se encontraron los datos de la imagen'
+            ];
+        }
+    
+        return response()->json($response, $response['status']);
+    }
+    
 }
